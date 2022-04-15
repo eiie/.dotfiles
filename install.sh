6f97650ca53c516a093835a4e3586c83f49904f5
Rémi <rdiep@osmozis.com>
2022-04-15 10:28:56 +0200

    tweaking vimrc

--- install.sh
+++ install.sh
@@ -1,50 +1,50 @@
 #!/usr/bin/env bash
 
 if [[ -d ~/.dotfiles  ]]; then
-    # vim
-    if [[ -d ~/.dotfiles/vim ]]; then
-	mkdir -p ~/.vim/{view,undo,spell,pack/plugins/{start,opt}}
-	for i in $(ls ~/.dotfiles/vim/plugins); do
-	    [[ -f "$i" ]] && ln -sf ~/.dotfiles/vim/plugins/"$i" ~/.vim/pack/plugins/start/"$i"
-	done
-	[[ -f ~/.dotfiles/vim/vimrc ]] && ln -sf ~/.dotfiles/vim/vimrc  ~/.vim/vimrc
-    fi
-
-    # bash
-    if [[ -d ~/.dotfiles/bash ]]; then
-	# bash_aliases
-	if [[ -f ~/.dotfiles/bash/bash_aliases ]]; then
-	    if [ -L ~/.bash_aliases ]; then
-		ln -sf ~/.dotfiles/bash/bash_aliases ~/.bash_aliases
-	    elif [ ! -f ~/.bash_aliases ]; then
-		ln -sf ~/.dotfiles/bash/bash_aliases ~/.bash_aliases
-	    else
-		while true; do
-		    read -p "A precedent configuration of .bash_aliases has been foud, do you want to erase it ? (Y/n)" yn
-		    yn=${yn:-y}
-		    case $yn in
-			[Yy]* ) ln -sf ~/.dotfiles/bash/bash_aliases ~/.bash_aliases; break;;
-			[Nn]* ) echo ".bash_aliases installation have been skipped" ; break;;
-			* ) continue;
-		    esac
+	# vim
+	if [[ -d ~/.dotfiles/vim ]]; then
+		mkdir -p ~/.vim/{view,undo,spell,pack/plugins/{start,opt}}
+		for i in $(ls ~/.dotfiles/vim/plugins); do
+			[[ -f "$i" ]] && ln -sf ~/.dotfiles/vim/plugins/"$i" ~/.vim/pack/plugins/start/"$i"
 		done
-	    fi
+		[[ -f ~/.dotfiles/vim/vimrc ]] && ln -sf ~/.dotfiles/vim/vimrc  ~/.vim/vimrc
 	fi
-    fi
-    # bashrc
-    #	if [[ -f ~/.dotfiles/bash/bashrc ]]; then
-    #	fi
-    # dunst
-    if [[ -d ~/.dotfiles/dunst ]]; then
-	mkdir -p ~/.config/dunst
-	[[ -f ~/.dotfiles/dunst/dunstrc ]] && ln -sf ~/.dotfiles/dunst/dunstrc ~/.config/dunst/dunstrc
-    fi
 
-    # readline
-    if [[ -d ~/.dotfiles/readline ]]; then
-	mkdir -p ~/.config/readline
-	[[ -f ~/.dotfiles/readline/inputrc ]] && ln -sf ~/.dotfiles/readline/inputrc  ~/.config/readline/inputrc
-    fi
+		# bash
+		if [[ -d ~/.dotfiles/bash ]]; then
+			# bash_aliases
+			if [[ -f ~/.dotfiles/bash/bash_aliases ]]; then
+				if [ -L ~/.bash_aliases ]; then
+					ln -sf ~/.dotfiles/bash/bash_aliases ~/.bash_aliases
+				elif [ ! -f ~/.bash_aliases ]; then
+					ln -sf ~/.dotfiles/bash/bash_aliases ~/.bash_aliases
+				else
+					while true; do
+						read -p "A precedent configuration of .bash_aliases has been foud, do you want to erase it ? (Y/n)" yn
+						yn=${yn:-y}
+						case $yn in
+							[Yy]* ) ln -sf ~/.dotfiles/bash/bash_aliases ~/.bash_aliases; break;;
+							[Nn]* ) echo ".bash_aliases installation have been skipped" ; break;;
+							* ) continue;
+						esac
+					done
+				fi
+			fi
+		fi
+		# bashrc
+		#	if [[ -f ~/.dotfiles/bash/bashrc ]]; then
+		#	fi
+		# dunst
+		if [[ -d ~/.dotfiles/dunst ]]; then
+			mkdir -p ~/.config/dunst
+			[[ -f ~/.dotfiles/dunst/dunstrc ]] && ln -sf ~/.dotfiles/dunst/dunstrc ~/.config/dunst/dunstrc
+		fi
+
+		# readline
+		if [[ -d ~/.dotfiles/readline ]]; then
+			mkdir -p ~/.config/readline
+			[[ -f ~/.dotfiles/readline/inputrc ]] && ln -sf ~/.dotfiles/readline/inputrc  ~/.config/readline/inputrc
+		fi
 fi
 
 # TODO
